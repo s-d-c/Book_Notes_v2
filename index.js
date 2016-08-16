13cabbd620c7f6be4d581afd019d221f2c1b334d
s-d-c <sdchute@gmail.com>
2016-08-16 08:58:40 -0700

    Begin restructure/workflow update

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var express = require('express');
 
 var app = express();
 
-//set up handlebars view engine
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
